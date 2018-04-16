fdc2df9bb242291a69fdfe044d66a448e9dcd1a3
201635700307 <461711605@qq.com>
2018-04-16 16:00:37 +0800

    task4

---
+++ task4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+	float x;
+	float y;
+	printf("Please enter x=");
+	scanf("%f",&x);
+	if (x<1)
+	{y=x;}
+	else if (x>=1&&x<10)
+	{y=2*x-1;}
+	else if (x>=10) 
+	{y=3*x-11;}
+	printf("y=%f",y);
+	return 0;
+}
+ 
